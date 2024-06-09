8c407756a95af3454f127bd15cb1ce5eced183c8
Afiq <afiq.student07@gmail.com>
2024-06-09 19:32:39 +0600

    first commit

---
+++ PART - 1/Tutorial-5.py
@@ -0,0 +1,14 @@
+food = "samosa";
+print(food[0])
+print(food[0:5])
+print(food[-1])
+print(len(food))
+
+a = "afiq , rifti";
+print("afiq" in a);
+print("Rifti" not in a);
+
+b=a.replace("afiq","Afiq")
+print(b)
+print(dir(b));
+print(a.upper())
---
+++ PART - 1/Tutorial.py
@@ -0,0 +1,8 @@
+a = 1.4567876543
+print(round(a,2))
+
+b = 4**4;
+print(b)
+
+c = format(5,'b')
+print(c)
